4522d494a92a0c62e249f4c49504c3e53fc7673c
Anton Gilgur <agilgur5@gmail.com>
2018-08-03 00:18:23 -0700

    (env): straight clone my front-end-base boilerplate
    - will make relevant changes in separate commits as necessary

---
+++ .babelrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'presets': [
+    ['@babel/preset-env', { useBuiltIns: 'usage' }],
+    '@babel/preset-react',
+    ['@babel/preset-stage-1', { decoratorsLegacy: true }]
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var commonEntry = ['./publicPath.es6', './polyfills.es6']
+// webpack's configuration
+module.exports = {
+  entry: {
+    main: commonEntry.concat('./main.es6')
+  },
+  output: {
+    path: __dirname + '/build', // where builds go
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    // resolve from package root as well, so utils/... , etc works
+    modules: [__dirname, 'node_modules']
+  },
+  module: {
+    rules: [
+      // use ! to chain loaders; note that the first loader is rightmost (RtL)
+      {
+        test: /\.es6$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.pcss$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      },
+      {
+        test: /\.pcssm$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1!postcss-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.cssm$/,
+        loader: 'style-loader!css-loader?modules'
+      },
+      // inline base64 URLs for <=8k images, direct URLs for the rest
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: 'url-loader?limit=8192'
+      }
+    ]
+  },
+  mode: process.env.NODE_ENV
+}
