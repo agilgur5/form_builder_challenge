928b7970b18f0c942c1668667265d8eca4034683
Anton Gilgur <agilgur5@gmail.com>
2018-08-04 23:52:04 -0700

    (optimize): use transform-import for smaller RBS bundling

--- .babelrc.js
+++ .babelrc.js
@@ -3,5 +3,13 @@ module.exports = {
     ['@babel/preset-env', { useBuiltIns: 'usage' }],
     '@babel/preset-react',
     ['@babel/preset-stage-1', { decoratorsLegacy: true }]
+  ],
+  'plugins': [
+    ['transform-imports', {
+      'react-bootstrap': {
+        'transform': 'react-bootstrap/lib/${member}',
+        'preventFullImport': true
+      }
+    }]
   ]
 }
